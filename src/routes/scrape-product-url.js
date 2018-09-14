9a9a6be8bc13fc6d05a2589696962a464136bcfe
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-09-14 12:29:31 -0400

    Implemented puppeteer

--- src/routes/scrape-product-url.js
+++ src/routes/scrape-product-url.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const puppeteer = require('puppeteer');
 
 // const authenticateJwt = require('../middleware/authenticate-jwt');
 const urlScraper = require('../utils/url-scraper');
@@ -20,10 +21,63 @@ const scrapeProductUrl = [
   }
 ];
 
+async function getImages(url) {
+  const browser = await puppeteer.launch({
+    headless: true
+  });
+
+  const page = await browser.newPage();
+
+  await page.setRequestInterception(true);
+
+  // Ignore loading images.
+  page.on('request', interceptedRequest => {
+    if (interceptedRequest.resourceType() === 'image') {
+      interceptedRequest.abort();
+    } else {
+      interceptedRequest.continue();
+    }
+  });
+
+  await page.goto(url);
+
+  const data = await page.evaluate(() => {
+    // TODO: Make this more specific for certain sites.
+    const images = document.querySelectorAll('img');
+
+    let result = [];
+    images.forEach((image) => {
+      const src = image.src;
+      // TODO: Add specific URL matchers to ignore certain images.
+      // (This one is for Amazon)
+      if (src && src.indexOf('/sprites/') === -1) {
+        result.push({ src });
+      }
+    });
+
+    return result;
+  });
+
+  await browser.close();
+
+  return data;
+}
+
 const router = express.Router();
-// router.use(authenticateJwt);
+
 router.route('/scrape-product-url')
   .get(scrapeProductUrl);
+
+router.route('/images')
+  .get((req, res, next) => {
+    const url = decodeURIComponent(req.query.url);
+
+    getImages(url)
+      .then((images) => {
+        res.json({ images });
+      })
+      .catch(next);
+  });
 
 module.exports = {
   middleware: {
