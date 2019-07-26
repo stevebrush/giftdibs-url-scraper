e7f9fa75bdf403684b2a9e6d5295af7bbf4f0879
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2019-07-26 10:10:02 -0400

    Removing `listen` because firebase handles that.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const app = require('./src/app');
 
-app.listen(app.get('port'), () => {
-  console.log(
-    `GiftDibs URL Scraper is listening on http://localhost:${app.get('port')}`
-  );
-});
+// app.listen(app.get('port'), () => {
+//   console.log(
+//     `GiftDibs URL Scraper is listening on http://localhost:${app.get('port')}`
+//   );
+// });
 
 // The `app` is referenced in firebase.json `rewrites`.
 const functions = require('firebase-functions');
