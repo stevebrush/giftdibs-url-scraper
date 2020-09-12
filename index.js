c867c5a1f98dac426bc313bd4e0b91267e31dcc9
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2020-09-11 21:21:04 -0400

    Upgrade packages

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
 const app = require('./src/app');
 
-// app.listen(app.get('port'), () => {
-//   console.log(
-//     `GiftDibs URL Scraper is listening on http://localhost:${app.get('port')}`
-//   );
-// });
-
 // The `app` is referenced in firebase.json `rewrites`.
 const functions = require('firebase-functions');
-exports.app = functions
-  .runWith({
-    memory: '2GB',
-    timeoutSeconds: 300
-  })
-  .https.onRequest(app);
+exports.app = functions.runWith({
+  memory: '2GB',
+  timeoutSeconds: 300
+}).https.onRequest(app);
